3d7bf90cb5ce6c4a9937ec9c5b70f673bafa85e7
flyeblue <flyeblue@hotmail.com>
2019-03-06 13:05:21 +0800

    first add

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # opencv401
 opencv 4.0.1 文档翻译
+利用omegat对opencv 4.0.1 文档进行翻译，主要翻译python部分
---
+++ source/4.0.1/search/enumvalues_16.js
@@ -0,0 +1,21 @@
+var searchData=
+[
+  ['value',['value',['../d7/d0e/classcv_1_1DataDepth.html#a310c72e217ddafe5496da760134a179aa7cb026110dca902585250642b85975d2',1,'cv::DataDepth::value()'],['../d6/d82/structcv_1_1cudev_1_1IsUnaryFunction.html#a7b931c49d40096c07889789fd2d6c907a15389267bc101f4e4513f9be55c7a7bc',1,'cv::cudev::IsUnaryFunction::value()'],['../d1/de5/structcv_1_1cudev_1_1IsBinaryFunction.html#a7f7cb8a6fb45187e2e1d826724fd20fea90ad3b693ab1636982b2edc8890cb66f',1,'cv::cudev::IsBinaryFunction::value()'],['../d9/d23/structcv_1_1cudev_1_1Int2Type.html#a69f1b460cf08a68334d380d997265ed2a30c67650242d5b240a90a7b989f03d16',1,'cv::cudev::Int2Type::value()'],['../d5/da6/structcv_1_1cudev_1_1Log2.html#a6c592979dad32824d0b3429b44e37da8af61f084c4b5cf2f65f4992b5ce3955c9',1,'cv::cudev::Log2::value()'],['../d5/d16/structcv_1_1cudev_1_1Log2_3_01N_00_010_00_01COUNT_01_4.html#a10197b0d4689de681f13e00ac7a7905fac667b581a71abad6bb3069a8626160d4',1,'cv::cudev::Log2&lt; N, 0, COUNT &gt;::value()'],['../da/db4/structcv_1_1cudev_1_1IsPowerOf2.html#a8c037131da23313f67f34a58bebe236dab32ea8ad86d2766541dbd8c20e29a9a6',1,'cv::cudev::IsPowerOf2::value()'],['../df/d71/structcv_1_1cudev_1_1TypesEquals.html#a7155ce03ada481abf6d6683a9b756c4da00d77df089f100b5179bb3ad3e4c412b',1,'cv::cudev::TypesEquals::value()'],['../df/dbf/structcv_1_1cudev_1_1TypesEquals_3_01A_00_01A_01_4.html#a7efdc9c0a148b7c4a3aaef75182b6a8ca68d076eb36da583e1fcca53401c00449',1,'cv::cudev::TypesEquals&lt; A, A &gt;::value()']]],
+  ['value_5fexpected',['VALUE_EXPECTED',['../da/d56/classcv_1_1FileStorage.html#a58c57b404290e3909b2ea93f7ae7ab4ea11b72f7c235089aebd2abdd72c0f7be9',1,'cv::FileStorage']]],
+  ['var_5fcategorical',['VAR_CATEGORICAL',['../dd/ded/group__ml.html#ggafd82a0e568907b8680027cb246a6eb06aa27f7cc3f72af77e1c2b39f5d91db078',1,'cv::ml']]],
+  ['var_5fnumerical',['VAR_NUMERICAL',['../dd/ded/group__ml.html#ggafd82a0e568907b8680027cb246a6eb06ae94fb869759383fce5f02a63b4666a3f',1,'cv::ml']]],
+  ['var_5fordered',['VAR_ORDERED',['../dd/ded/group__ml.html#ggafd82a0e568907b8680027cb246a6eb06a077b19eb913ac28b0a7ca765943cd188',1,'cv::ml']]],
+  ['vc1',['VC1',['../d0/d61/group__cudacodec.html#gga71943a1181287609b5d649f53ce6c146ad8aeb35f84f90985490476b6b305d70f',1,'cv::cudacodec']]],
+  ['vendor_5famd',['VENDOR_AMD',['../d7/d9f/classcv_1_1ocl_1_1Device.html#ababfa74ca7cd6055c5fdabe67f4e138ea0740a4cbb837d501241473afb8f315ce',1,'cv::ocl::Device']]],
+  ['vendor_5fintel',['VENDOR_INTEL',['../d7/d9f/classcv_1_1ocl_1_1Device.html#ababfa74ca7cd6055c5fdabe67f4e138ea88cb8f15bc0a45beab394555f8a7d074',1,'cv::ocl::Device']]],
+  ['vendor_5fnvidia',['VENDOR_NVIDIA',['../d7/d9f/classcv_1_1ocl_1_1Device.html#ababfa74ca7cd6055c5fdabe67f4e138ea63e352d43b9750291dd897deff5d1ba2',1,'cv::ocl::Device']]],
+  ['vgg_5f120',['VGG_120',['../d6/d00/classcv_1_1xfeatures2d_1_1VGG.html#abcd2a789a969520678813e5c1259fdcaab354dfdb4f6db979000d3c9bf96b6b04',1,'cv::xfeatures2d::VGG']]],
+  ['vgg_5f48',['VGG_48',['../d6/d00/classcv_1_1xfeatures2d_1_1VGG.html#abcd2a789a969520678813e5c1259fdcaa30fa9c0525aa047ff290216fa68d06ff',1,'cv::xfeatures2d::VGG']]],
+  ['vgg_5f64',['VGG_64',['../d6/d00/classcv_1_1xfeatures2d_1_1VGG.html#abcd2a789a969520678813e5c1259fdcaa2a743f83c3244f76802fee45cbdf048d',1,'cv::xfeatures2d::VGG']]],
+  ['vgg_5f80',['VGG_80',['../d6/d00/classcv_1_1xfeatures2d_1_1VGG.html#abcd2a789a969520678813e5c1259fdcaaef818dea56d8f9ec497c7816ea0cf453',1,'cv::xfeatures2d::VGG']]],
+  ['videowriter_5fprop_5fframebytes',['VIDEOWRITER_PROP_FRAMEBYTES',['../d4/d15/group__videoio__flags__base.html#gga41c5cfa7859ae542b71b1d33bbd4d2b4ace3320e146f4f95f3d58b32b0e1237b1',1,'cv']]],
+  ['videowriter_5fprop_5fnstripes',['VIDEOWRITER_PROP_NSTRIPES',['../d4/d15/group__videoio__flags__base.html#gga41c5cfa7859ae542b71b1d33bbd4d2b4ae8e8348e4b6b8c78d9921c65c91a8e10',1,'cv']]],
+  ['videowriter_5fprop_5fquality',['VIDEOWRITER_PROP_QUALITY',['../d4/d15/group__videoio__flags__base.html#gga41c5cfa7859ae542b71b1d33bbd4d2b4a1cc2ab47f60a115ff9624ff71715b93b',1,'cv']]],
+  ['voronoi_5fseam',['VORONOI_SEAM',['../d7/d09/classcv_1_1detail_1_1SeamFinder.html#a69d548f204d956fae9b09c1b8cfc7cf0a55b2503305e94168c0b36c4531f288d7',1,'cv::detail::SeamFinder']]],
+  ['vscroll',['VScroll',['../dc/d3b/classcv_1_1viz_1_1MouseEvent.html#ada240f2528500ad1615e76fb2fd9e2baabbfe491796298420cd5eefc79fb2ed17',1,'cv::viz::MouseEvent']]]
+];
